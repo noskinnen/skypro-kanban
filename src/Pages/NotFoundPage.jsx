3ad982f244b4434d4ce1d08f364a73d77df0736d
Andreas Noskinnen <130252943+noskinnen@users.noreply.github.com>
2024-03-09 20:45:27 +0300

    Add files via upload

---
+++ src/Pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage404() {
+  return (
+      <div> <iframe src="https://gifer.com/embed/7iJR">
+        </iframe><p>
+          <a href="https://gifer.com">СТРАНИЦА НЕ НАЙДЕНА</a></p></div>
+  )
+}
